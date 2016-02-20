d960cfb0ca671492ca88984b47b6583758df9f4b
Alexander Trost <galexrt@googlemail.com>
2016-02-20 15:16:44 +0100

    Install bash and use bash not sh

--- entrypoint-win.sh
+++ entrypoint-win.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 Xvfb :0 -screen 0 800x600x16 &
 
@@ -6,4 +6,4 @@ Xvfb :0 -screen 0 800x600x16 &
 # xinit "$DATA_DIR" -- /usr/bin/Xvfb :0 -screen 0 800x600x16 -ac &
 
 export DISPLAY=:0.0
-exec /bin/sh "$@"
+exec /bin/bash "$@"
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
 # TODO
 
-exec /bin/sh "$@"
+exec /bin/bash "$@"
